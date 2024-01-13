7449e8f742c895f6c1d2c1534d12d64ceea8978f
Rashadmlkv <resadmlkv@gmail.com>
2024-01-12 22:49:26 -0800

    code style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,10 +1,14 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_full - checks binary tree is full
-* @tree: root
-* Return: 1 yes, 0 NO
-*/
+ * binary_tree_is_full - Checks if a binary tree is full.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: if node is full 1, otherwise 0.
+ * If tree is NULL, return 0.
+ */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -12,4 +16,36 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (2 * binary_tree_leaves(tree) - 1 == binary_tree_size(tree))
 		return (1);
 	return (0);
+}
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Number of leaves, if tree is NULL returns 0.
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Size of tree, if tree NULL returns 0.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
